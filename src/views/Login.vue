d2d886afed44faa0acef59e606ea54e09e6471d5
Theo Sherman <theo.a.sherman@gmail.com>
2018-05-17 14:05:22 -0500

    Adding social logins.

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -2,22 +2,37 @@
 v-content
   v-container(fluid fill-height)
     v-layout(align-center justify-center)
-      v-flex(xs12 sm8 md4)
+      v-flex(sm12 md6)
         v-card.elevation-12
-          v-toolbar(dark color='primary')
-            v-toolbar-title Login
           v-card-text
             v-alert(:value='!!errorMessage' outline color='error' icon='warning' style='margin-bottom: 20px;') {{ errorMessage }}
-            v-form
-              v-text-field(v-model='email', prepend-icon='person', label='Email', type='text')
-              v-text-field(v-model='password', prepend-icon='lock', label='Password', type='password')
-          v-card-actions
-            v-spacer
-            v-btn(@click='login()' color='primary') Login
+
+            v-container(fluid)
+              v-layout
+
+                v-flex(md5)
+                  v-card.elevation-0
+                    v-card-text
+                      v-btn(dark color='red') Login with Google
+
+                v-flex(md1)
+                  v-layout(align-center justify-center)
+                    v-flex(xs12)
+                      h2 OR
+
+                v-flex(md6)
+                  v-card.elevation-0
+                    v-card-text
+                      v-form
+                        v-text-field(v-model='email', prepend-icon='person', label='Email', type='text')
+                        v-text-field(v-model='password', prepend-icon='lock', label='Password', type='password')
+                    v-card-actions
+                      v-spacer
+                      v-btn(@click='login()' color='primary') Login
+            
 </template>
 
 <script>
-//import { get } from 'vuex-pathify'
 import firebase from '@firebase/app'
 
 export default {
@@ -29,9 +44,8 @@ export default {
   methods: {
     login() {
       firebase.auth().signInWithEmailAndPassword(this.email, this.password)
-      .then(() => {
-        this.email = this.password = ''
-      }).catch(error => { this.errorMessage = error.message })
+        .then(() => { this.$router.push('/') })
+        .catch(error => { this.errorMessage = error.message })
     }
   }
 }
